0e647ac07674b55ce87cdaf1f0fad0f0b74a6b3c
manikaa86 <starmanikamoon@gmail.com>
2022-11-10 23:17:28 -0600

    searchbar & button

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1> Welcome!</h1>
       </header>
+      <form className="search">
+        <input type="text" placeholder="Search for a recipe" />
+        <input className="submit" type="submit" value="Go"></input>
+      </form>
     </div>
   );
 }
