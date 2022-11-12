9ba8f26d1be921d1a7404ea75663d30c1c58fffa
manikaa86 <starmanikamoon@gmail.com>
2022-11-11 23:17:59 -0600

    text functionality

--- src/App.js
+++ src/App.js
@@ -14,15 +14,27 @@ function App() {
       <header className="App-header">
         <h1> Welcome!</h1>
       </header>
+      <center>
+        <div class="typed-out">
+          Welcome to Food Recipe Search, the best place to find recipes that
+          meet your nutritional needs.
+        </div>
+      </center>
+      &nbsp;
+      {/* need to fix button functionality */}
       <div className="side-buttons">
-        <button> Profile </button>
-        <button> Recipe Search </button>
+        <center>
+          <button> Profile </button>
+          <button> Recipe Search </button>{" "}
+        </center>
       </div>
       <center>
+        &nbsp;
         <div className="card">
           <label>
-            <b> Recipe of the Week </b>
+            <b> Recipe of the Week: </b>
           </label>
+          <p> Spaghetti Carbonara! Click on the image to learn more</p>
           <center>
             <a href="https://www.google.com/url?sa=i&url=https%3A%2F%2Fcooking.nytimes.com%2Frecipes%2F12965-spaghetti-carbonara&psig=AOvVaw2DTUdLEqB8hq_kiUeTBmwv&ust=1668315004431000&source=images&cd=vfe&ved=0CA8QjRxqFwoTCLCN-evrp_sCFQAAAAAdAAAAABAD">
               <img
@@ -30,11 +42,11 @@ function App() {
                 width="299"
                 height="210"
               />
-            </a>{" "}
+            </a>
           </center>
         </div>
+        &nbsp;
       </center>
-
       <form className="search">
         <input type="text" placeholder="Search for a recipe" />
         <input className="submit" type="submit" value="Go"></input>
