a09f835e352b2e6d21ddcf276e950d79ed41119b
Manika <starmanikamoon@gmail.com>
2022-11-30 12:37:41 -0600

    added login functionality for success criterias 1 & 7

--- src/App.js
+++ src/App.js
@@ -3,48 +3,47 @@ import "./App.css";
 import {
   BrowserRouter as Router,
   Routes,
-  Route,
-  Navigate,
+  Route
 } from "react-router-dom";
+import { useForm } from "react-hook-form";
 import Home from "./components/Home";
 import Profile from "./components/Profile";
 import RecipeSearch from "./components/RecipeSearch";
-
-// const rootElement = document.getElementById("root");
-// const root = createRoot(rootElement);
+import Login from "./Login";
 
 function App() {
-  const navigateProfile = () => {
-    console.log("navigated to profile");
+
+  const {register, handleSubmit, formState: { errors } } = useForm();
+  const onSubmit = (data) => {
+    localStorage.setItem(data.email, JSON.stringify({ 
+        name: data.name, password: data.password 
+    }));
+    console.log(JSON.parse(localStorage.getItem(data.email)));
   };
 
-  const navigateSearch = () => {
-    console.log("navigated to search");
-  };
 
   return (
     <div className="App">
       <>
         <Router>
           <Routes>
-            <Route exact path="/" element={<Home />} />
+            <Route path="/" element={<Home />} />
             <Route path="/Profile" element={<Profile />} />
             <Route path="/RecipeSearch" element={<RecipeSearch />} />
+            <Route exact path="/login" element={<Login />}> </Route>
           </Routes>
         </Router>
       </>
-      &nbsp;
-      {/* need to fix button functionality */}
-      {/* <div className="side-buttons">
-        <center>
-          <button onClick={navigateProfile}> Profile </button>
-          <button onClick={navigateSearch}> Recipe Search </button>{" "}
-        </center>
-      </div> */}
+      <p className="title">Registration Form</p>
       <center>
-        &nbsp;
-        
-        &nbsp;
+        <form className="App" onSubmit={handleSubmit(onSubmit)}>
+          <input type="text" {...register("name")} />
+          <input type="email" {...register("email", { required: true })} />
+          {errors.email && <span style={{ color: "red" }}>
+          Please enter your email to proceed </span>}
+          <input type="password" {...register("password")} />
+          <input type={"submit"} style={{ backgroundColor: "#a1eafb" }} />
+        </form>
       </center>
     </div>
   );
---
+++ src/Login.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { useForm } from "react-hook-form";
+import "./App.css";
+
+function Login() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    const userData = JSON.parse(localStorage.getItem(data.email));
+    if (userData) {
+      if (userData.password === data.password) {
+        console.log(userData.name + "You are now logged in");
+      } else {
+        console.log("Email and Passowrd do not match");
+      }
+    } else {
+      console.log("Email and Passowrd do not match");
+    }
+  };
+  return (
+    <>
+      <p className="title">Login Form</p>
+
+      <form className="App" onSubmit={handleSubmit(onSubmit)}>
+        <input type="email" {...register("email", { required: true })} />
+        {errors.email && <span style={{ color: "red" }}>
+         *Email* is mandatory </span>}
+        <input type="password" {...register("password")} />
+        <input type={"submit"} style={{ backgroundColor: "#a1eafb" }} />
+      </form>
+    </>
+  );
+}
+export default Login;
