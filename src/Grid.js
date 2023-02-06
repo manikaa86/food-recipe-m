99e2aca15393a7e2f354a43fa59b6bf43c58bd5c
Manika <starmanikamoon@gmail.com>
2023-02-06 12:39:36 -0600

    linked recipe results to api (complexity)

--- src/Grid.js
+++ src/Grid.js
@@ -4,7 +4,10 @@ import "./Grid.css";
 export default function Grid({ recipe }) {
   return (
     <div className="grid">
-      <img className="recipe-image" src={recipe["recipe"]["image"]} />
+      {/* <img className="recipe-image" src={recipe["recipe"]["image"]} /> */}
+
+      <p><a href= {recipe["recipe"]["shareAs"]}><img src={recipe["recipe"]["image"]}/></a></p>
+
       <p className="recipe-name"> {recipe["recipe"]["label"]} </p>
     </div>
   );
