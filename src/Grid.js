a6549b5462a3272fd8276bfd0150ff43dd543fc3
manikaa86 <starmanikamoon@gmail.com>
2023-03-16 13:22:38 -0500

    recipe saving button

--- src/Grid.js
+++ src/Grid.js
@@ -1,11 +1,31 @@
-import React from "react";
+import { useState } from "react";
 import "./Grid.css";
+import "@fortawesome/fontawesome-free/css/all.css";
 
 export default function Grid({ recipe }) {
+  const [saved, setSaved] = useState(false);
+
+  const handleSaveClick = () => {
+    setSaved(!saved);
+  };
+
   return (
     <div className="grid">
-      <p><a href= {recipe["recipe"]["shareAs"]}><img className="recipe-image" src={recipe["recipe"]["image"]}/></a></p>
-      <p className="recipe-name"> {recipe["recipe"]["label"]} </p>
+      <p>
+        <a href={recipe["recipe"]["shareAs"]}>
+          <img className="recipe-image" src={recipe["recipe"]["image"]} />
+        </a>
+      </p>
+      <p className="recipe-name">
+        {recipe["recipe"]["label"]}{" "}
+        <button
+          className={`save-button ${saved ? "saved" : ""}`}
+          onClick={handleSaveClick}
+          title={saved ? "Saved" : "Save"}
+        >
+          <i className="fa fa-star"></i>
+        </button>
+      </p>
     </div>
   );
 }
